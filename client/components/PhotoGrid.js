b07288a5ce6febf96ed83e557f899b3c97ea010f
basheer <basheer@4acetech.com>
2017-12-07 10:14:56 +0500

    Initial Commit

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+    render() {
+        return (
+            <div>
+                Photo Grid section
+            </div>
+        )
+    }
+});
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+    render() {
+        return (
+            <div>
+                SinglePhoto section
+            </div>
+        )
+    }
+});
+
+export default Single;
